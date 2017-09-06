5a049ec5458e26b40fd95537a2742220f672f4c7
Daniel Sager <software@sager1.de>
2017-09-06 17:33:29 +0200

    Re-order & memoize analyze methods

--- lib/article_json/import/google_doc/html/node.rb
+++ lib/article_json/import/google_doc/html/node.rb
@@ -10,12 +10,6 @@ module ArticleJSON
             @node = node
           end
 
-          # Check if the node is a header tag between <h1> and <h5>
-          # @return [Boolean]
-          def heading?
-            %w(h1 h2 h3 h4 h5).include?(node.name)
-          end
-
           # Check if a node equals a certain text
           # @param [String] text
           # @return [Boolean]
@@ -28,40 +22,53 @@ module ArticleJSON
           # sure that it's not an image.
           # @return [Boolean]
           def empty?
-            node.inner_text.strip.empty? && !image?
+            return @is_empty if defined? @is_empty
+            @is_empty = node.inner_text.strip.empty? && !image?
           end
 
-          # Check if the node contains an image
+          # Check if the node is a header tag between <h1> and <h5>
           # @return [Boolean]
-          def image?
-            node.xpath('.//img').length > 0
+          def heading?
+            return @is_heading if defined? @is_heading
+            @is_heading = %w(h1 h2 h3 h4 h5).include?(node.name)
           end
 
           # Check if the node contains an ordered or unordered list
           # @return [Boolean]
           def list?
-            %w(ul ol).include?(node.name)
+            return @is_list if defined? @is_list
+            @is_list = %w(ul ol).include?(node.name)
           end
 
           # Check if the node starts a text box
           # Text boxes start with a single line saying "Textbox:".
           # @return [Boolean]
           def text_box?
-            has_text?('textbox:')
+            return @is_text_box if defined? @is_text_box
+            @is_text_box = has_text?('textbox:')
           end
 
           # Check if the node starts a highlight box
           # Story highlights start with a single line saying "Highlight:".
           # @return [Boolean]
           def highlight?
-            has_text?('highlight:')
+            return @is_highlight if defined? @is_highlight
+            @is_highlight = has_text?('highlight:')
           end
 
           # Check if the node starts a quote
           # Quotes start with a single line saying "Quote:".
           # @return [Boolean]
           def quote?
-            has_text?('quote:')
+            return @is_quote if defined? @is_quote
+            @is_quote = has_text?('quote:')
+          end
+
+          # Check if the node contains an image
+          # @return [Boolean]
+          def image?
+            return @is_image if defined? @is_image
+            @is_image = node.xpath('.//img').length > 0
           end
 
           # Determine the type of this node
@@ -69,12 +76,12 @@ module ArticleJSON
           # @return [Symbol]
           def type
             return :empty if empty?
-            return :image if image?
+            return :heading if heading?
+            return :list if list?
             return :text_box if text_box?
             return :highlight if highlight?
             return :quote if quote?
-            return :heading if heading?
-            return :list if list?
+            return :image if image?
             :text
           end
         end
