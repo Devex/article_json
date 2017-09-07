b62fceeda04992572bf6444ec78c04d270aa224f
Daniel Sager <software@sager1.de>
2017-09-07 13:04:25 +0200

    Add text elements
    Text elements are the basic unit of content, paragraphs will be
    composed of multiple text elements...

--- lib/article_json.rb
+++ lib/article_json.rb
@@ -1,3 +1,6 @@
+require 'uri'
+require 'cgi'
+
 require 'nokogiri'
 require 'css_parser'
 
@@ -5,3 +8,4 @@ require_relative 'article_json/version'
 
 require_relative 'article_json/import/google_doc/html/css_analyzer'
 require_relative 'article_json/import/google_doc/html/node_analyzer'
+require_relative 'article_json/import/google_doc/html/text_element'
