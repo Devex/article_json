3b21c107536bcbe6620cbcf3f45fba577456a8e2
Daniel Sager <software@sager1.de>
2017-09-07 19:12:05 +0200

    Add heading element
    A heading is created from a `h1`, `h2`, `h3`, `h4` or `h5` tag.

---
+++ lib/article_json/import/google_doc/html/heading_element.rb
@@ -0,0 +1,43 @@
+module ArticleJSON
+  module Import
+    module GoogleDoc
+      module HTML
+        class HeadingElement
+          # @param [Nokogiri::XML::Node] node
+          def initialize(node:)
+            @node = node
+          end
+
+          # The raw text content of the heading, without any markup
+          # @return [String]
+          def content
+            @node.inner_text
+          end
+
+          # Determine the level of the heading
+          # The level corresponds to the header tag, e.g. `<h3>` is level 3.
+          # @return [Integer]
+          def level
+            case @node.name
+              when 'h1' then 1
+              when 'h2' then 2
+              when 'h3' then 3
+              when 'h4' then 4
+              when 'h5' then 5
+            end
+          end
+
+          # Hash representation of this heading element
+          # @return [Hash]
+          def to_h
+            {
+              type: :heading,
+              level: level,
+              content: content
+            }
+          end
+        end
+      end
+    end
+  end
+end
