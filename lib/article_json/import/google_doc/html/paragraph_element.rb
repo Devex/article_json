071e86248952accaeff1a7859fc77f45393c0076
Daniel Sager <software@sager1.de>
2017-09-07 14:28:52 +0200

    Add paragraph element
    A paragraphs is just a collection of text elements without any
    specific features or attributes.

---
+++ lib/article_json/import/google_doc/html/paragraph_element.rb
@@ -0,0 +1,30 @@
+module ArticleJSON
+  module Import
+    module GoogleDoc
+      module HTML
+        class ParagraphElement
+          # @param [Nokogiri::XML::Node] node
+          # @param [ArticleJSON::Import::GoogleDoc::HTML::CSSAnalyzer] css_analyzer
+          def initialize(node:, css_analyzer:)
+            @node = node
+            @css_analyzer = css_analyzer
+          end
+
+          # @return [Array[ArticleJSON::Import::GoogleDoc::HTML::TextElement]]
+          def content
+            TextElement.extract(text_node: @node, css_analyzer: @css_analyzer)
+          end
+
+          # Hash representation of this paragraph element
+          # @return [Hash]
+          def to_h
+            {
+              type: :paragraph,
+              content: content.map(&:to_h)
+            }
+          end
+        end
+      end
+    end
+  end
+end
