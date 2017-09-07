5b10ddb7e954e0ff5f4c0bcb3a7188d14973866f
Daniel Sager <software@sager1.de>
2017-09-07 10:53:09 +0200

    Rename CSSParser to CSSAnalyzer
    Also remove the `is_` prefix from style-check methods...

--- lib/article_json.rb
+++ lib/article_json.rb
@@ -3,5 +3,5 @@ require 'css_parser'
 
 require_relative 'article_json/version'
 
-require_relative 'article_json/import/google_doc/html/css_parser'
+require_relative 'article_json/import/google_doc/html/css_analyzer'
 require_relative 'article_json/import/google_doc/html/node'
