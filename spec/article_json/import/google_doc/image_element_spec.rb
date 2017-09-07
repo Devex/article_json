508ff52ba4910ccf9f1c18df6cd4c73be90e65bb
Daniel Sager <software@sager1.de>
2017-09-07 18:28:27 +0200

    Add image element
    An image is a node wrapping an `img` tag and another node that
    contains the caption, treated as text elements.

---
+++ spec/article_json/import/google_doc/image_element_spec.rb
@@ -0,0 +1,166 @@
+describe ArticleJSON::Import::GoogleDoc::HTML::ImageElement do
+  subject(:element) do
+    described_class.new(
+      node: node,
+      caption_node: caption_node,
+      css_analyzer: css_analyzer
+    )
+  end
+  let(:node) do
+    Nokogiri::XML.fragment(image_fragment.strip).first_element_child
+  end
+  let(:caption_node) do
+    Nokogiri::XML.fragment(caption_fragment.strip).first_element_child
+  end
+  let(:css_analyzer) do
+    ArticleJSON::Import::GoogleDoc::HTML::CSSAnalyzer.new(css)
+  end
+  let(:css) { '' }
+  let(:source_url) { 'foo/bar.jpg' }
+  let(:image_fragment) { "<p><span><img src=\"#{source_url}\"></span></p>" }
+  let(:caption_fragment) { '<p><span>foo</span></p>' }
+
+  describe '#source_url' do
+    subject { element.source_url }
+    it { should eq source_url }
+  end
+
+  describe '#float' do
+    subject { element.float }
+
+    let(:css) do
+      <<-css
+        .center { text-align: center }
+        .right { text-align: right }
+      css
+    end
+    let(:image_fragment) do
+      "<p class=\"#{p_class}\"><span>#{img_tag}</span></p>"
+    end
+
+    context 'for a image without width' do
+      let(:img_tag) { '<img src="foo/bar">' }
+
+      context 'and without alignment' do
+        let(:p_class) { '' }
+        it { should be nil }
+      end
+
+      context 'and with right-alignment' do
+        let(:p_class) { 'right' }
+        it { should be nil }
+      end
+
+      context 'and with left-alignment' do
+        let(:p_class) { 'center' }
+        it { should be nil }
+      end
+    end
+
+    context 'for a image with a width-attribute' do
+      context 'and not wider than 500px' do
+        let(:img_tag) { '<img width="400" src="foo/bar">' }
+
+        context 'and without alignment' do
+          let(:p_class) { '' }
+          it { should be :left }
+        end
+
+        context 'and with right-alignment' do
+          let(:p_class) { 'right' }
+          it { should be :right }
+        end
+
+        context 'and with left-alignment' do
+          let(:p_class) { 'center' }
+          it { should be nil }
+        end
+      end
+
+      context 'and wider than 500px' do
+        let(:img_tag) { '<img width="700" src="foo/bar">' }
+
+        context 'and without alignment' do
+          let(:p_class) { '' }
+          it { should be nil }
+        end
+
+        context 'and with right-alignment' do
+          let(:p_class) { 'right' }
+          it { should be nil }
+        end
+
+        context 'and with left-alignment' do
+          let(:p_class) { 'center' }
+          it { should be nil }
+        end
+      end
+    end
+
+    context 'for a image with a style-attribute' do
+      context 'and a width wider than 500px' do
+        let(:img_tag) { '<img style="width: 400px;" src="foo/bar">' }
+
+        context 'and without alignment' do
+          let(:p_class) { '' }
+          it { should be :left }
+        end
+
+        context 'and with right-alignment' do
+          let(:p_class) { 'right' }
+          it { should be :right }
+        end
+
+        context 'and with left-alignment' do
+          let(:p_class) { 'center' }
+          it { should be nil }
+        end
+      end
+
+      context 'and wider than 500px' do
+        let(:img_tag) { '<img style="width: 700px;" src="foo/bar">' }
+
+        context 'and without alignment' do
+          let(:p_class) { '' }
+          it { should be nil }
+        end
+
+        context 'and with right-alignment' do
+          let(:p_class) { 'right' }
+          it { should be nil }
+        end
+
+        context 'and with left-alignment' do
+          let(:p_class) { 'center' }
+          it { should be nil }
+        end
+      end
+    end
+  end
+
+  describe '#caption' do
+    subject { element.caption }
+
+    it 'returns a list of text elements' do
+      expect(subject).to be_an Array
+      expect(subject.size).to eq 1
+
+      expect(subject)
+        .to all be_a ArticleJSON::Import::GoogleDoc::HTML::TextElement
+
+      expect(subject.first.content).to eq 'foo'
+    end
+  end
+
+  describe 'to_h' do
+    subject { element.to_h }
+
+    it 'returns a proper Hash' do
+      expect(subject).to be_a Hash
+      expect(subject[:type]).to eq :image
+      expect(subject[:source_url]).to eq source_url
+      expect(subject[:float]).to be nil
+      expect(subject[:caption]).to eq element.caption.map(&:to_h)
+    end
+  end
+end
