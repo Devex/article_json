7f73f0be16983babe3b31fe7d75fc9b4c718bb49
Daniel Sager <software@sager1.de>
2017-09-08 16:45:08 +0200

    Add quote element
    A quote can contains multiple paragraphs and a caption, which is
    the last node.

---
+++ lib/article_json/import/google_doc/html/quote_element.rb
@@ -0,0 +1,54 @@
+module ArticleJSON
+  module Import
+    module GoogleDoc
+      module HTML
+        class QuoteElement
+          # @param [Array[Nokogiri::XML::Node]] nodes
+          # @param [ArticleJSON::Import::GoogleDoc::HTML::CSSAnalyzer] css_analyzer
+          def initialize(nodes:, css_analyzer:)
+            @nodes = nodes.reject { |node| NodeAnalyzer.new(node).empty? }
+            @css_analyzer = css_analyzer
+          end
+
+          # Check if the quote is floating (left, right or not at all)
+          # The first paragraph of the quote indicates its floating value.
+          # @return [Symbol]
+          def float
+            node_class = @nodes.first.attribute('class')&.value || ''
+            return :right if @css_analyzer.right_aligned?(node_class)
+            return :left if @css_analyzer.left_aligned?(node_class)
+            nil
+          end
+
+          # Parse the quote's nodes to get a set of paragraphs
+          # The last node is ignored as it contains the quote caption
+          # @return [Array]
+          def content
+            @nodes
+              .take(@nodes.size - 1)
+              .map do |node|
+                ParagraphElement.new(node: node, css_analyzer: @css_analyzer)
+              end
+          end
+
+          # Parse the quote's last node to get the caption
+          # @return [Array[ArticleJSON::Import::GoogleDoc::HTML::TextElement]]
+          def caption
+            TextElement.extract(node: @nodes.last, css_analyzer: @css_analyzer)
+          end
+
+          # Hash representation of this quote
+          # @return [Hash]
+          def to_h
+            {
+              type: :quote,
+              float: float,
+              content: content.map(&:to_h),
+              caption: caption.map(&:to_h),
+            }
+          end
+        end
+      end
+    end
+  end
+end
---
+++ spec/article_json/import/google_doc/quote_element_spec.rb
@@ -0,0 +1,78 @@
+describe ArticleJSON::Import::GoogleDoc::HTML::QuoteElement do
+  subject(:element) do
+    described_class.new(nodes: node.children, css_analyzer: css_analyzer)
+  end
+
+  let(:node) { Nokogiri::XML.fragment(html.strip) }
+  let(:html) do
+    <<-html
+      <p class="css_class"><span>#{quote}</span></p>
+      <p class="css_class"><span>#{caption}</span></p>
+    html
+  end
+  let(:quote) { 'Operator! Give me the number for 911!' }
+  let(:caption) { 'Homer Simpson' }
+
+  let(:css_analyzer) do
+    ArticleJSON::Import::GoogleDoc::HTML::CSSAnalyzer.new(css)
+  end
+  let(:css) { '' }
+
+  describe '#float' do
+    subject { element.float }
+    context 'when the first node is centered' do
+      let(:css) { '.css_class { text-align: center }' }
+      it { should be nil }
+    end
+
+    context 'when the first node is right aligned' do
+      let(:css) { '.css_class { text-align: right }' }
+      it { should be :right }
+    end
+
+    context 'when the first node is not aligned' do
+      it { should be :left }
+    end
+  end
+
+  describe '#content' do
+    subject { element.content }
+
+    it 'returns a list of paragraph and heading elements' do
+      expect(subject).to be_an Array
+      expect(subject.size).to eq 1
+
+      expect(subject.first)
+        .to be_a ArticleJSON::Import::GoogleDoc::HTML::ParagraphElement
+      expect(subject.first.content.first.content).to eq quote
+    end
+  end
+
+  describe '#caption' do
+    subject { element.caption }
+
+    it 'returns a list of text elements' do
+      expect(subject).to be_an Array
+      expect(subject.size).to eq 1
+
+      expect(subject.first)
+        .to be_a ArticleJSON::Import::GoogleDoc::HTML::TextElement
+
+      expect(subject.first.content).to eq caption
+    end
+  end
+
+  describe '#to_h' do
+    subject { element.to_h }
+
+    it 'returns a proper Hash' do
+      expect(subject).to be_a Hash
+      expect(subject[:type]).to eq :quote
+      expect(subject[:float]).to be :left
+      expect(subject[:content]).to be_an Array
+      expect(subject[:content]).to all be_a Hash
+      expect(subject[:caption]).to be_an Array
+      expect(subject[:caption]).to all be_a Hash
+    end
+  end
+end
