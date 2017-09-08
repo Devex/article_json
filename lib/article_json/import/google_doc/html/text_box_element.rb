5510aae5629a6932043a21a1a01cae914917de61
Daniel Sager <software@sager1.de>
2017-09-08 12:07:36 +0200

    Add text box element
    A text box can contain headings and paragraphs, in the future they
    should also support lists.

---
+++ lib/article_json/import/google_doc/html/text_box_element.rb
@@ -0,0 +1,54 @@
+module ArticleJSON
+  module Import
+    module GoogleDoc
+      module HTML
+        class TextBoxElement
+          # @param [Array[Nokogiri::XML::Node]] nodes
+          # @param [ArticleJSON::Import::GoogleDoc::HTML::CSSAnalyzer] css_analyzer
+          def initialize(nodes:, css_analyzer:)
+            @nodes = nodes.reject { |node| NodeAnalyzer.new(node).empty? }
+            @css_analyzer = css_analyzer
+          end
+
+          # Check if the text box is floating (left, right or not at all)
+          # The first paragraph of the text box indicates its floating value.
+          # @return [Symbol]
+          def float
+            node_class = @nodes.first.attribute('class')&.value || ''
+            return :right if @css_analyzer.right_aligned?(node_class)
+            return :left if @css_analyzer.left_aligned?(node_class)
+            nil
+          end
+
+          # Parse the text box's nodes to get a list of sub elements
+          # Supported sub elements are: headings & paragraphs
+          # @return [Array]
+          def content
+            @nodes.map { |node| parse_sub_node(node) }.compact
+          end
+
+          # Hash representation of this text box
+          # @return [Hash]
+          def to_h
+            {
+              type: :text_box,
+              float: float,
+              content: content.map(&:to_h),
+            }
+          end
+
+          private
+
+          def parse_sub_node(node)
+            case NodeAnalyzer.new(node).type
+            when :heading then
+              HeadingElement.new(node: node)
+            when :paragraph then
+              ParagraphElement.new(node: node, css_analyzer: @css_analyzer)
+            end
+          end
+        end
+      end
+    end
+  end
+end
---
+++ spec/article_json/import/google_doc/text_box_element_spec.rb
@@ -0,0 +1,69 @@
+describe ArticleJSON::Import::GoogleDoc::HTML::TextBoxElement do
+  subject(:element) do
+    described_class.new(nodes: node.children, css_analyzer: css_analyzer)
+  end
+
+  let(:node) { Nokogiri::XML.fragment(html.strip) }
+  let(:html) do
+    <<-html
+      <h2 class="css_class"><span>This is a text box!</span></h2>
+      <p><span>With a paragraph.</span></p>
+      <p><span>And another one...</span></p>
+    html
+  end
+
+  let(:css_analyzer) do
+    ArticleJSON::Import::GoogleDoc::HTML::CSSAnalyzer.new(css)
+  end
+  let(:css) { '' }
+
+  describe '#float' do
+    subject { element.float }
+    context 'when the first node is centered' do
+      let(:css) { '.css_class { text-align: center }' }
+      it { should be nil }
+    end
+
+    context 'when the first node is right aligned' do
+      let(:css) { '.css_class { text-align: right }' }
+      it { should be :right }
+    end
+
+    context 'when the first node is not aligned' do
+      it { should be :left }
+    end
+  end
+
+  describe '#content' do
+    subject { element.content }
+
+    it 'returns a list of paragraph and heading elements' do
+      expect(subject).to be_an Array
+      expect(subject.size).to eq 3
+
+      expect(subject[0])
+        .to be_a ArticleJSON::Import::GoogleDoc::HTML::HeadingElement
+      expect(subject[0].content).to eq 'This is a text box!'
+
+      expect(subject[1])
+        .to be_a ArticleJSON::Import::GoogleDoc::HTML::ParagraphElement
+      expect(subject[1].content.first.content).to eq 'With a paragraph.'
+
+      expect(subject[2])
+        .to be_a ArticleJSON::Import::GoogleDoc::HTML::ParagraphElement
+      expect(subject[2].content.first.content).to eq 'And another one...'
+    end
+  end
+
+  describe '#to_h' do
+    subject { element.to_h }
+
+    it 'returns a proper Hash' do
+      expect(subject).to be_a Hash
+      expect(subject[:type]).to eq :text_box
+      expect(subject[:float]).to eq :left
+      expect(subject[:content]).to be_an Array
+      expect(subject[:content]).to all be_a Hash
+    end
+  end
+end
