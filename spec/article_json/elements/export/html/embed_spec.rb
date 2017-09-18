986e100f434d5793b1d0be5820da9373a65c81c4
Daniel Sager <software@sager1.de>
2017-09-18 16:08:02 +0200

    Add HTML export for embed elements
    This is just a stub, until embedded objects are resolved
    properly via OEmbed calls.

---
+++ spec/article_json/elements/export/html/embed_spec.rb
@@ -0,0 +1,21 @@
+describe ArticleJSON::Export::HTML::Elements::Embed do
+  subject(:element) { described_class.new(source_element) }
+
+  let(:source_element) do
+    ArticleJSON::Elements::Embed.new(
+      embed_type: :something,
+      embed_id: 666,
+      caption: [ArticleJSON::Elements::Text.new(content: 'Foo Bar')],
+      tags: %w(test)
+    )
+  end
+
+  describe '#build' do
+    subject { element.build.to_html(save_with: 0) }
+    let(:expected_html) do
+      '<figure><div class="embed">Embedded Object: something-666</div>' \
+        '<figcaption>Foo Bar</figcaption></figure>'
+    end
+    it { should eq expected_html }
+  end
+end
