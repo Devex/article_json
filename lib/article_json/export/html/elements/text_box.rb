0719d598dd16629d2a3c72ab780cb91d863b82f2
Daniel Sager <software@sager1.de>
2017-09-18 15:05:09 +0200

    Add HTML export for text box elements

---
+++ lib/article_json/export/html/elements/text_box.rb
@@ -0,0 +1,25 @@
+module ArticleJSON
+  module Export
+    module HTML
+      module Elements
+        class TextBox < Base
+          include Shared::Float
+
+          def build
+            create_element(:div, node_opts).tap do |div|
+              @element.content.each do |child_element|
+                div.add_child(Base.new(child_element).build)
+              end
+            end
+          end
+
+          private
+
+          def node_opts
+            { class: ['text-box', floating_class].compact.join(' ') }
+          end
+        end
+      end
+    end
+  end
+end
