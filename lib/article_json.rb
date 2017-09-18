dd0a822a84bd47cdcddf0750e205c6890ead2df8
Daniel Sager <software@sager1.de>
2017-09-18 13:55:32 +0200

    Add HTML export for image elements
    Adding two modules to share the logic around floating and captions.

--- lib/article_json.rb
+++ lib/article_json.rb
@@ -36,8 +36,11 @@ require_relative 'article_json/import/google_doc/html/embedded_slideshare_parser
 require_relative 'article_json/import/google_doc/html/embedded_tweet_parser'
 require_relative 'article_json/import/google_doc/html/parser'
 
+require_relative 'article_json/export/html/elements/shared/caption'
+require_relative 'article_json/export/html/elements/shared/float'
 require_relative 'article_json/export/html/elements/base'
 require_relative 'article_json/export/html/elements/text'
 require_relative 'article_json/export/html/elements/heading'
 require_relative 'article_json/export/html/elements/paragraph'
 require_relative 'article_json/export/html/elements/list'
+require_relative 'article_json/export/html/elements/image'
