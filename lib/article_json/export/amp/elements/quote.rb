5de2769a612795c59fba42814b6a716b35976fe4
Manu Campos <manu@invisibles.cc>
2017-09-29 11:56:41 +0200

    Add AMP quote element

---
+++ lib/article_json/export/amp/elements/quote.rb
@@ -0,0 +1,10 @@
+module ArticleJSON
+  module Export
+    module AMP
+      module Elements
+        class Quote < ::ArticleJSON::Export::HTML::Elements::Quote
+        end
+      end
+    end
+  end
+end
