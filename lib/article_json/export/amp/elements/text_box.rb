51cd2861637b035052fb0721dfe11adb66150bbe
Manu Campos <manu@invisibles.cc>
2017-10-02 15:45:48 +0200

    Add AMP TextBox Element

---
+++ lib/article_json/export/amp/elements/text_box.rb
@@ -0,0 +1,25 @@
+module ArticleJSON
+  module Export
+    module AMP
+      module Elements
+        class TextBox < Base
+          include Shared::Float
+
+          def export
+            create_element(:div, node_opts).tap do |div|
+              @element.content.each do |child_element|
+                div.add_child(Base.new(child_element).export)
+              end
+            end
+          end
+
+          private
+
+          def node_opts
+            { class: ['text-box', floating_class].compact.join(' ') }
+          end
+        end
+      end
+    end
+  end
+end
