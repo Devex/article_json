d9e5467f5f42eaee2dd79311fc54ed39ee9ef55f
Manu Campos <manu@invisibles.cc>
2017-10-04 18:11:37 +0200

    Add SlideShare AMP support

--- spec/article_json/export/amp/elements/embed_spec.rb
+++ spec/article_json/export/amp/elements/embed_spec.rb
@@ -93,5 +93,35 @@ describe ArticleJSON::Export::AMP::Elements::Embed do
 
       it { should eq expected_html }
     end
+
+    context 'with a slideshare presentation' do
+      let(:source_element) do
+        ArticleJSON::Elements::Embed.new(
+          embed_type: :slideshare,
+          embed_id: 666,
+          caption: [ArticleJSON::Elements::Text.new(content: 'Foo Bar')],
+          tags: %w(test)
+        )
+      end
+      let(:expected_html) do
+        '<figure><div class="embed">' \
+        '<amp-iframe ' \
+        'src="https://www.slideshare.net/slideshow/embed_code/key/fAYvedL9rNCr9k"' \
+        ' width="427" height="356" frameborder="0">' \
+        '</amp-iframe></div><figcaption>Foo Bar</figcaption></figure>'
+      end
+
+      let(:oembed_data) do
+        JSON.parse(
+          File.read('spec/fixtures/slideshare_oembed.json'),
+          symbolize_names: 1
+        )
+      end
+
+      before do
+        allow(source_element).to receive(:oembed_data).and_return(oembed_data)
+      end
+      it { should eq expected_html }
+    end
   end
 end
