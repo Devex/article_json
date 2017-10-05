49bfab68e0e59217623ee9b7cc09b6f01c00c93e
Manu Campos <manu@invisibles.cc>
2017-10-05 17:52:02 +0200

    List all amp javascript files needed

--- lib/article_json/export/amp/elements/embed.rb
+++ lib/article_json/export/amp/elements/embed.rb
@@ -4,19 +4,13 @@ module ArticleJSON
       module Elements
         class Embed < Base
           include Shared::Caption
+          attr_reader :amp_library
 
           def export
+            @amp_library = nil
             create_element(:figure).tap do |figure|
               figure.add_child(embed_node)
               figure.add_child(caption_node(:figcaption))
-            end
-          end
-
-          private
-
-          def embed_node
-            create_element(:div, class: 'embed').tap do |div|
-              div.add_child(type_specific_node)
             end
           end
 
@@ -35,28 +29,60 @@ module ArticleJSON
             end
           end
 
+          private
+
+          def embed_node
+            create_element(:div, class: 'embed').tap do |div|
+              div.add_child(type_specific_node)
+            end
+          end
+
+          def youtube_library
+            '<script async custom-element="amp-youtube" ' \
+            'src="https://cdn.ampproject.org/v0/amp-youtube-0.1.js"></script>'
+          end
+
           def youtube_node
+            @amp_library = youtube_library
             create_element('amp-youtube',
                            'data-videoid' => @element.embed_id,
                            width: default_width,
                            height: default_height)
           end
 
+          def vimeo_library
+            '<script async custom-element="amp-vimeo"' \
+            'src="https://cdn.ampproject.org/v0/amp-vimeo-0.1.js"></script>'
+          end
+
           def vimeo_node
+            @amp_library = vimeo_library
             create_element('amp-vimeo',
                            'data-videoid' => @element.embed_id,
                            width: default_width,
                            height: default_height)
           end
 
+          def tweet_library
+            '<script async custom-element="amp-twitter" ' \
+            'src="https://cdn.ampproject.org/v0/amp-twitter-0.1.js"></script>'
+          end
+
           def tweet_node
+            @amp_library = tweet_library
             create_element('amp-twitter',
                            'data-tweetid' => @element.embed_id,
                            width: default_width,
                            height: default_height)
           end
 
+          def facebook_library
+            '<script async custom-element="amp-facebook" ' \
+            'src="https://cdn.ampproject.org/v0/amp-facebook-0.1.js"></script>'
+          end
+
           def facebook_node
+            @amp_library = facebook_library
             url = "#{@element.oembed_data[:author_url]}videos/#{@element.embed_id}"
             create_element('amp-facebook',
                            'data-embedded-as' => 'video',
@@ -65,7 +91,13 @@ module ArticleJSON
                            height: default_height)
           end
 
+          def iframe_library
+            '<script async custom-element="amp-iframe"' \
+            'src="https://cdn.ampproject.org/v0/amp-iframe-0.1.js"></script>'
+          end
+
           def slideshare_node
+            @amp_library = iframe_library
             node = Nokogiri::HTML(@element.oembed_data[:html]).xpath('//iframe')
             create_element('amp-iframe',
                            src: node.attribute('src').value,
--- spec/article_json/export/amp/exporter_spec.rb
+++ spec/article_json/export/amp/exporter_spec.rb
@@ -9,4 +9,29 @@ describe ArticleJSON::Export::AMP::Exporter do
     before { stub_oembed_requests }
     it { should eq html.strip }
   end
+
+  describe 'amp_libraries' do
+    subject { exporter.amp_libraries }
+
+    let(:json) { File.read('spec/fixtures/reference_document_parsed.json') }
+    let(:elements) { ArticleJSON::Article.from_json(json).elements }
+    let(:expected_result) do
+      [
+        '<script async custom-element="amp-vimeo"' \
+        'src="https://cdn.ampproject.org/v0/amp-vimeo-0.1.js"></script>',
+        '<script async custom-element="amp-youtube" ' \
+        'src="https://cdn.ampproject.org/v0/amp-youtube-0.1.js"></script>',
+        '<script async custom-element="amp-facebook" ' \
+        'src="https://cdn.ampproject.org/v0/amp-facebook-0.1.js"></script>',
+        '<script async custom-element="amp-iframe"' \
+        'src="https://cdn.ampproject.org/v0/amp-iframe-0.1.js"></script>',
+        '<script async custom-element="amp-twitter" ' \
+        'src="https://cdn.ampproject.org/v0/amp-twitter-0.1.js"></script>'
+      ]
+    end
+
+    before { stub_oembed_requests }
+
+    it { should eq expected_result }
+  end
 end
