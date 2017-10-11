53ae9ca481787cd6eb889886e09ae8600fd1cf2a
Nicolas Fricke <nicolas.fricke@devex.com>
2017-10-11 19:06:14 +0200

    Add generic way for defining custom exporters in configuration
    By adding `ArticleJSON::Configuration#register_element_exporters_for`
    we can in the future allow the user to use the same method to also add
    custom exporters e.g. for AMP.
    
    Adapt and deprecate the existing methods so that they still work as they
    currently do but can be removed in the future.

--- spec/article_json/configuration_spec.rb
+++ spec/article_json/configuration_spec.rb
@@ -43,6 +43,64 @@ describe ArticleJSON::Configuration do
     end
   end
 
+  describe '#register_element_exporters_for' do
+    subject do
+      ArticleJSON.configure do |c|
+        c.register_element_exporters_for(exporter, mapping)
+      end
+    end
+    let(:exporter) { :html }
+    let(:mapping) { {} }
+
+    context 'when passed an incorrect exporter' do
+      context 'when exporter is `nil`' do
+        let(:exporter) { nil }
+
+        it 'should raise an ArgumentError' do
+          expect { subject }.to raise_error ArgumentError, /exporter/
+        end
+      end
+
+      context 'when exporter is `:foobar`' do
+        let(:exporter) { :foobar }
+
+        it 'should raise an ArgumentError' do
+          expect { subject }.to raise_error ArgumentError, /exporter/
+        end
+      end
+    end
+
+    context 'when passed an incorrect mapping' do
+      shared_examples 'for an ArgumentError due to incorrect mapping' do
+        it 'should raise an ArgumentError' do
+          expect { subject }.to raise_error ArgumentError, /type_class_mapping/
+        end
+      end
+
+      context 'which has non-symbol keys' do
+        let(:mapping) { { 42 => Object } }
+        include_examples 'for an ArgumentError due to incorrect mapping'
+      end
+
+      context 'which has non-class values' do
+        let(:mapping) { { image: 42 } }
+        include_examples 'for an ArgumentError due to incorrect mapping'
+      end
+    end
+
+    context 'with valid arguments' do
+      let(:exporter) { :html }
+      let(:mapping) { { image: Object, text: String } }
+      let(:config) { ArticleJSON.configuration }
+
+      it 'registers the correct additional exporters' do
+        expect { subject }
+          .to change { config.exporter_for(:html, :image) }.from(nil).to(Object)
+          .and change { config.exporter_for(:html, :text) }.from(nil).to(String)
+      end
+    end
+  end
+
   describe '#oembed_user_agent' do
     subject { configuration.oembed_user_agent }
 
