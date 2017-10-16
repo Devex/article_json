a12710467b20fb468339f7e33f769f2c5088826b
Nicolas Fricke <nicolas.fricke@devex.com>
2017-10-16 20:05:39 +0200

    Add support for placing additional elements into article
    The new method `Article#place_additional_elements` allows a user to
    place additional elements into the article, like e.g. advertisements.
    These elements need to be defined within the user's own code.
    
    Each element that is added this way will directly get placed in between
    paragraphs of the article. The method ensures that an additional element
    is never added before or after any node other than paragraphs (e.g. an
    image). The elements are added in the order you pass them into the
    method. If the article should not have enough spaces to place all the
    provided elements, they will be placed after the last element in the
    article.
    
    Usage examples are provided in the updated `README.md`.

---
+++ spec/article_json/utils/additional_element_placer_spec.rb
@@ -0,0 +1,203 @@
+describe ArticleJSON::Utils::AdditionalElementPlacer do
+  describe '#merge_elements' do
+    subject { described_class.new(article, additional_elements).merge_elements }
+    let(:paragraph) { ArticleJSON::Elements::Paragraph.new(content: 'text') }
+    let(:image) do
+      ArticleJSON::Elements::Image.new(source_url: 'url', caption: 'caption')
+    end
+    let(:quote) do
+      ArticleJSON::Elements::Quote.new(content: 'uh la la', caption: 'caption')
+    end
+    let(:list) { ArticleJSON::Elements::List.new(content: '1 2 3') }
+    let(:embed) do
+      ArticleJSON::Elements::Embed.new(embed_type: 'youtube',
+                                       embed_id: 'qwe123',
+                                       caption: 'oh so funny')
+    end
+
+    let(:article_elements) do
+      [
+        paragraph,
+        image,
+        image,
+        paragraph,
+        # Possible additional element position
+        paragraph,
+        # Possible additional element position
+        paragraph,
+        # Possible additional element position
+        paragraph,
+        # Possible additional element position
+        paragraph,
+        # Possible additional element position
+        paragraph,
+        quote,
+        quote,
+        paragraph,
+        # Possible additional element position
+        paragraph,
+        embed,
+      ]
+    end
+    let(:article) { ArticleJSON::Article.new(article_elements) }
+
+    shared_examples 'for properly added additional elements' do
+      it 'should place the elements in the right position' do
+        expect(subject.map(&:type)).to eq expected_article_elements.map(&:type)
+      end
+    end
+
+    context 'if the article has enough possible positions' do
+      let(:element_1) { double('additional_element_1', type: :element_1) }
+      let(:element_2) { double('additional_element_2', type: :element_2) }
+      let(:element_3) { double('additional_element_3', type: :element_3) }
+      let(:additional_elements) { [element_1, element_2, element_3] }
+
+      include_examples 'for properly added additional elements' do
+        let(:expected_article_elements) do
+          [
+            paragraph,
+            image,
+            image,
+            paragraph,
+            # Possible additional element position
+            paragraph,
+            element_1, # inserted element
+            paragraph,
+            # Possible additional element position
+            paragraph,
+            element_2, # inserted element
+            paragraph,
+            # Possible additional element position
+            paragraph,
+            quote,
+            quote,
+            paragraph,
+            element_3, # inserted element
+            paragraph,
+            embed,
+          ]
+        end
+      end
+    end
+
+    context 'if the article has more possible positions' do
+      let(:element_1) { double('additional_element_1', type: :element_1) }
+      let(:element_2) { double('additional_element_2', type: :element_2) }
+      let(:element_3) { double('additional_element_3', type: :element_3) }
+      let(:additional_elements) { [element_1, element_2, element_3] }
+
+      include_examples 'for properly added additional elements' do
+        let(:expected_article_elements) do
+          [
+            paragraph,
+            image,
+            image,
+            paragraph,
+            # Possible additional element position
+            paragraph,
+            element_1, # inserted element
+            paragraph,
+            # Possible additional element position
+            paragraph,
+            element_2, # inserted element
+            paragraph,
+            # Possible additional element position
+            paragraph,
+            quote,
+            quote,
+            paragraph,
+            element_3, # inserted element
+            paragraph,
+            embed,
+          ]
+        end
+      end
+    end
+
+    context 'if the article has just enough possible positions' do
+      let(:element_1) { double('additional_element_1', type: :element_1) }
+      let(:element_2) { double('additional_element_2', type: :element_2) }
+      let(:element_3) { double('additional_element_3', type: :element_3) }
+      let(:element_4) { double('additional_element_4', type: :element_4) }
+      let(:element_5) { double('additional_element_5', type: :element_5) }
+      let(:element_6) { double('additional_element_6', type: :element_6) }
+      let(:additional_elements) do
+        [element_1, element_2, element_3, element_4, element_5, element_6]
+      end
+
+      include_examples 'for properly added additional elements' do
+        let(:expected_article_elements) do
+          [
+            paragraph,
+            image,
+            image,
+            paragraph,
+            element_1, # inserted element
+            paragraph,
+            element_2, # inserted element
+            paragraph,
+            element_3, # inserted element
+            paragraph,
+            element_4, # inserted element
+            paragraph,
+            element_5, # inserted element
+            paragraph,
+            quote,
+            quote,
+            paragraph,
+            element_6, # inserted element
+            paragraph,
+            embed,
+          ]
+        end
+      end
+    end
+
+    context 'if the article does not have enough possible positions' do
+      let(:element_1) { double('additional_element_1', type: :element_1) }
+      let(:element_2) { double('additional_element_2', type: :element_2) }
+      let(:element_3) { double('additional_element_3', type: :element_3) }
+      let(:element_4) { double('additional_element_4', type: :element_4) }
+      let(:element_5) { double('additional_element_5', type: :element_5) }
+      let(:element_6) { double('additional_element_6', type: :element_6) }
+      let(:element_7) { double('additional_element_7', type: :element_7) }
+      let(:element_8) { double('additional_element_8', type: :element_8) }
+      let(:element_9) { double('additional_element_9', type: :element_9) }
+      let(:additional_elements) do
+        [element_1, element_2, element_3, element_4, element_5, element_6,
+         element_7, element_8, element_9]
+      end
+
+      include_examples 'for properly added additional elements' do
+        let(:expected_article_elements) do
+          [
+            paragraph,
+            image,
+            image,
+            paragraph,
+            element_1, # inserted element
+            paragraph,
+            element_2, # inserted element
+            paragraph,
+            element_3, # inserted element
+            paragraph,
+            element_4, # inserted element
+            paragraph,
+            element_5, # inserted element
+            paragraph,
+            quote,
+            quote,
+            paragraph,
+            element_6, # inserted element
+            paragraph,
+            embed,
+            element_7, # inserted element at the end
+            element_8, # inserted element at the end
+            element_9, # inserted element at the end
+          ]
+        end
+      end
+    end
+  end
+end
