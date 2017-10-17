f3eae360570f436c80b2cc269ca92858a52f9ad8
Nicolas Fricke <nicolas.fricke@devex.com>
2017-10-17 18:21:27 +0200

    Add `Paragraph#empty?` method: test if it has any content

--- spec/article_json/elements/paragraph_spec.rb
+++ spec/article_json/elements/paragraph_spec.rb
@@ -1,13 +1,52 @@
 describe ArticleJSON::Elements::Paragraph do
   subject(:element) { described_class.new(**params) }
-  let(:params) { { content: [content] } }
-  let(:content) { ArticleJSON::Elements::Text.new(content: 'Foo Bar') }
-  let(:hash) { params.merge(type: :paragraph, content: [content.to_h]) }
+  let(:params) { { content: content } }
+  let(:content) { [ArticleJSON::Elements::Text.new(content: 'Foo Bar')] }
+  let(:hash) { params.merge(type: :paragraph, content: content.map(&:to_h)) }
 
   describe '#to_h' do
     subject { element.to_h }
     it { should be_a Hash }
     it { should eq hash }
+  end
+
+  describe '#empty?' do
+    subject { element.empty? }
+
+    context 'when `content` contains only text elements' do
+      context 'when some text elements contain actual text' do
+        let(:content) do
+          [
+            ArticleJSON::Elements::Text.new(content: ''),
+            ArticleJSON::Elements::Text.new(content: 'Lorem ipsum?'),
+            ArticleJSON::Elements::Text.new(content: ' '),
+          ]
+        end
+        it { should be false }
+      end
+
+      context 'when all text elements are blank' do
+        let(:content) do
+          [
+            ArticleJSON::Elements::Text.new(content: ''),
+            ArticleJSON::Elements::Text.new(content: ' '),
+          ]
+        end
+        it { should be false }
+      end
+    end
+
+    context 'when `content` contains other elements' do
+      let(:content) do
+        [ArticleJSON::Elements::Image.new(source_url: 'foo.jpg', caption: [])]
+      end
+      it { should be false }
+    end
+
+    context 'when `content` is `nil`' do
+      let(:content) { nil }
+      it { should be true }
+    end
   end
 
   describe '.parse_hash' do
