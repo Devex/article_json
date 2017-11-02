08e42337f4917dfdbd7d7c3a59c2255a86d32175
Nicolas Fricke <nicolas.fricke@devex.com>
2017-11-02 19:18:37 +0100

    Extract common code from HTML and AMP `Embed` exporter
    AMP is basically just a custom version of HTML. Therefore, the code of
    the two exporters is very similar. For better reusability (e.g. when
    implementing a Facebook Instant Article exporter), extract the code into
    a `Common` module which then is reused by both classes.

---
+++ lib/article_json/export/common/html/elements/embed.rb
@@ -0,0 +1,34 @@
+module ArticleJSON
+  module Export
+    module Common
+      module HTML
+        module Elements
+          module Embed
+            include ArticleJSON::Export::Common::HTML::Elements::Shared::Caption
+
+            # Generate the embedded element node
+            # @return [Nokogiri::XML::NodeSet]
+            def export
+              create_element(:figure) do |figure|
+                figure.add_child(embed_node)
+                figure.add_child(caption_node(:figcaption))
+              end
+            end
+
+            private
+
+            def embed_node
+              create_element(:div, class: 'embed') do |div|
+                div.add_child(embedded_object)
+              end
+            end
+
+            def embedded_object
+              Nokogiri::HTML.fragment(@element.oembed_data[:html])
+            end
+          end
+        end
+      end
+    end
+  end
+end
