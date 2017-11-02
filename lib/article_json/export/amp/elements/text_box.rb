728b5bf3ede9390a10ea662acf1df040eded4fb8
Nicolas Fricke <nicolas.fricke@devex.com>
2017-11-02 19:11:49 +0100

    Extract common code from HTML and AMP `TextBox` exporter
    AMP is basically just a custom version of HTML. Therefore, the code of
    the two exporters is very similar. For better reusability (e.g. when
    implementing a Facebook Instant Article exporter), extract the code into
    a `Common` module which then is reused by both classes.

--- lib/article_json/export/amp/elements/text_box.rb
+++ lib/article_json/export/amp/elements/text_box.rb
@@ -3,23 +3,7 @@ module ArticleJSON
     module AMP
       module Elements
         class TextBox < Base
-          include ArticleJSON::Export::Common::HTML::Elements::Shared::Float
-
-          # Generate a `<div>` node containing all text box elements
-          # return [Nokogiri::XML::NodeSet]
-          def export
-            create_element(:div, node_opts) do |div|
-              @element.content.each do |child_element|
-                div.add_child(Base.new(child_element).export)
-              end
-            end
-          end
-
-          private
-
-          def node_opts
-            { class: ['text-box', floating_class].compact.join(' ') }
-          end
+          include ArticleJSON::Export::Common::HTML::Elements::TextBox
         end
       end
     end
