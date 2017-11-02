195f6d9e708f25ef60816d4c5f24cf4aaea4e6b6
Nicolas Fricke <nicolas.fricke@devex.com>
2017-11-02 16:51:18 +0100

    Extract common code from HTML and AMP `Text` exporters
    AMP is basically just a custom version of HTML. Therefore, the code of
    the two exporters is very similar. For better reusability (e.g. when
    implementing a Facebook Instant Article exporter), extract the code into
    a `Common` module which then is reused by both classes.

--- lib/article_json/export/amp/elements/text.rb
+++ lib/article_json/export/amp/elements/text.rb
@@ -3,51 +3,7 @@ module ArticleJSON
     module AMP
       module Elements
         class Text < Base
-          # Generate a Nokogiri node or simple text node, depending on the text
-          # options
-          # @return [Nokogiri::XML::NodeSet]
-          def export
-            return bold_and_italic_node if @element.bold && @element.italic
-            return bold_node if @element.bold
-            return italic_node if @element.italic
-            content_node
-          end
-
-          private
-
-          # @return [Nokogiri::XML::NodeSet]
-          def italic_node
-            create_element(:em) { |em| em.add_child(content_node) }
-          end
-
-          # @return [Nokogiri::HTML::Node]
-          def bold_node
-            create_element(:strong) do |strong|
-              strong.add_child(content_node)
-            end
-          end
-
-          # @return [Nokogiri::HTML::Node]
-          def bold_and_italic_node
-            create_element(:strong) do |strong|
-              strong.add_child(italic_node)
-            end
-          end
-
-          # @return [Nokogiri::XML::NodeSet]
-          def content_node
-            return create_text_nodes(@element.content) if @element.href.nil?
-            create_element(:a, href: @element.href) do |a|
-              a.add_child(create_text_nodes(@element.content))
-            end
-          end
-
-          # @param [Nokogiri::HTML::NodeSet] text
-          def create_text_nodes(text)
-            Nokogiri::HTML
-              .fragment(text.gsub(/\n/, '<br>'))
-              .children
-          end
+          include ArticleJSON::Export::Common::HTML::Elements::Text
         end
       end
     end
