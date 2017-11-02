971200b01ad219326d3cbd6863045ff87b2fd069
Nicolas Fricke <nicolas.fricke@devex.com>
2017-11-02 17:12:11 +0100

    Extract common code from HTML and AMP `List` exporter
    AMP is basically just a custom version of HTML. Therefore, the code of
    the two exporters is very similar. For better reusability (e.g. when
    implementing a Facebook Instant Article exporter), extract the code into
    a `Common` module which then is reused by both classes.

--- lib/article_json.rb
+++ lib/article_json.rb
@@ -43,6 +43,7 @@ require_relative 'article_json/export/common/html/elements/base'
 require_relative 'article_json/export/common/html/elements/text'
 require_relative 'article_json/export/common/html/elements/heading'
 require_relative 'article_json/export/common/html/elements/paragraph'
+require_relative 'article_json/export/common/html/elements/list'
 
 require_relative 'article_json/export/html/elements/shared/caption'
 require_relative 'article_json/export/html/elements/shared/float'
--- lib/article_json/export/amp/elements/list.rb
+++ lib/article_json/export/amp/elements/list.rb
@@ -3,24 +3,7 @@ module ArticleJSON
     module AMP
       module Elements
         class List < Base
-          # Generate the list node with its elements
-          # @return [Nokogiri::XML::NodeSet]
-          def export
-            create_element(tag_name) do |list|
-              @element.content.each do |child_element|
-                list_item_wrapper = create_element(:li) do |item|
-                  item.add_child(Paragraph.new(child_element).export)
-                end
-                list.add_child(list_item_wrapper)
-              end
-            end
-          end
-
-          private
-
-          def tag_name
-            @element.list_type == :ordered ? :ol : :ul
-          end
+          include ArticleJSON::Export::Common::HTML::Elements::List
         end
       end
     end
