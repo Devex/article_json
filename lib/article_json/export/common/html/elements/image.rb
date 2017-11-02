5fe44cbe894e502ab95fe1765677607cd18a5a37
Nicolas Fricke <nicolas.fricke@devex.com>
2017-11-02 19:14:13 +0100

    Extract common code from HTML and AMP `Image` exporter
    AMP is basically just a custom version of HTML. Therefore, the code of
    the two exporters is very similar. For better reusability (e.g. when
    implementing a Facebook Instant Article exporter), extract the code into
    a `Common` module which then is reused by both classes.

---
+++ lib/article_json/export/common/html/elements/image.rb
@@ -0,0 +1,36 @@
+module ArticleJSON
+  module Export
+    module Common
+      module HTML
+        module Elements
+          module Image
+            include ArticleJSON::Export::Common::HTML::Elements::Shared::Caption
+            include ArticleJSON::Export::Common::HTML::Elements::Shared::Float
+
+            # Generate the `<figure>` node containing the image and caption
+            # @return [Nokogiri::XML::Element]
+            def export
+              create_element(:figure, node_opts) do |figure|
+                figure.add_child(image_node)
+                figure.add_child(caption_node(:figcaption))
+              end
+            end
+
+            private
+
+            # @return [Nokogiri::XML::NodeSet]
+            def image_node
+              create_element(:img, src: @element.source_url)
+            end
+
+            # @return [Hash]
+            def node_opts
+              return if floating_class.nil?
+              { class: floating_class }
+            end
+          end
+        end
+      end
+    end
+  end
+end
