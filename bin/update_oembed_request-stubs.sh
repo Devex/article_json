c51547d80a84fb67a83995d2b9c9af34e4fa9727
Manu Campos <manu@invisibles.cc>
2020-03-23 16:08:21 +0100

    Add a script to update oembed stubs
    Adds a script to update the fixtures for the stubbed requests to oembed for the
    different services we use.
    
    This way we keep the fixtures up to date for our tests.

---
+++ bin/update_oembed_request-stubs.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env sh
+
+curl -X GET "https://vimeo.com/api/oembed.json?url=https://vimeo.com/42315417" | jq > spec/fixtures/vimeo_video_oembed.json
+
+curl -X GET "http://www.youtube.com/oembed?format=json&url=https://www.youtube.com/watch?v=_ZG8HBuDjgc" | jq > spec/fixtures/youtube_video_oembed.json
+
+curl -X GET "https://www.slideshare.net/api/oembed/2?format=json&url=https://www.slideshare.net/Devex/the-best-global-development-quotes-of-2012" |  jq > spec/fixtures/slideshare_oembed.json
+
+curl -X GET "https://api.twitter.com/1/statuses/oembed.json?align=center&url=https://twitter.com/d3v3x/status/554608639030599681" | jq > spec/fixtures/tweet_oembed.json
+
+curl -X GET 'https://soundcloud.com/oembed?format=json&url=https://soundcloud.com/rich-the-kid/plug-walk-1' | jq > spec/fixtures/soundcloud_oembed.json
+
+curl -X GET  'https://www.facebook.com/plugins/video/oembed.json?url=https://www.facebook.com/facebook/videos/1814600831891266'  | jq > spec/fixtures/facebook_video_oembed.json
