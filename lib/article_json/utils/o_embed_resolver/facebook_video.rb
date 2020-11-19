bbab59b55a0e3115877ba5a7d1e7dcf20fad7214
Manu Campos <manu@invisibles.cc>
2020-11-19 13:42:31 +0100

    Update facebook oembed support
    We add the option to access the new facebook oembed endpoints, which are not
    public anymore. To achieve this, we add an option in the configuration class to
    add the token we need to access this endpoints.

--- lib/article_json/utils/o_embed_resolver/facebook_video.rb
+++ lib/article_json/utils/o_embed_resolver/facebook_video.rb
@@ -11,13 +11,29 @@ module ArticleJSON
         # The URL for the oembed API call
         # @return [String]
         def oembed_url
-          "https://www.facebook.com/plugins/video/oembed.json?url=#{source_url}"
+          "https://graph.facebook.com/v9.0/oembed_video?url=#{source_url}" \
+          "&access_token=#{access_token}"
         end
 
         # The video URL of the element
         # @return [String]
         def source_url
           "https://www.facebook.com/facebook/videos/#{@element.embed_id}"
+        end
+
+        # The facebook access token. If not set, it raises an exception
+        # explaining how to configure it.
+        #
+        # @return [String]
+        def access_token
+          token = ArticleJSON.configuration.facebook_token
+
+          if token.nil?
+            raise 'You need to configure the facebook token to use facebook' \
+                  'embed videos, see:' \
+                  'https://github.com/Devex/article_json#facebook-oembed'
+          end
+          token
         end
       end
     end
--- spec/article_json/utils/o_embed_resolver/facebook_video_spec.rb
+++ spec/article_json/utils/o_embed_resolver/facebook_video_spec.rb
@@ -1,21 +1,49 @@
 require_relative 'oembed_resolver_shared'
 
 describe ArticleJSON::Utils::OEmbedResolver::FacebookVideo do
-  include_context 'for a successful oembed resolution' do
-    let(:element) do
-      ArticleJSON::Elements::Embed.new(
-        embed_type: :facebook_video,
-        embed_id: 1814600831891266,
-        caption: []
-      )
+  let(:element) do
+    ArticleJSON::Elements::Embed.new(
+      embed_type: :facebook_video,
+      embed_id: 1814600831891266,
+      caption: []
+    )
+  end
+
+  context 'when the config has the token' do
+    before do
+      ArticleJSON.configure { |c| c.facebook_token = 'fake_facebook_token' }
     end
-    let(:expected_oembed_url) do
-      'https://www.facebook.com/plugins/video/oembed.json?url=' \
-        'https://www.facebook.com/facebook/videos/1814600831891266'
+
+    include_context 'for a successful oembed resolution' do
+      let(:expected_oembed_url) do
+        'https://graph.facebook.com/v9.0/oembed_video?url=' \
+        'https://www.facebook.com/facebook/videos/1814600831891266' \
+        '&access_token=fake_facebook_token'
+      end
+      let(:oembed_response) do
+        File.read('spec/fixtures/facebook_video_oembed.json')
+      end
+      let(:expected_name) { 'Facebook video' }
     end
-    let(:oembed_response) do
-      File.read('spec/fixtures/facebook_video_oembed.json')
+  end
+
+  describe '#access_token' do
+    subject { described_class.build(element).access_token }
+
+    context 'when the config has not the token' do
+      before { ArticleJSON.configure { |c| c.facebook_token = nil } }
+
+      it 'raises an error' do
+        expect { subject }.to raise_error StandardError
+      end
     end
-    let(:expected_name) { 'Facebook video' }
+
+    context 'with a proper config token' do
+      let(:token) { 'fake_facebook_token' }
+
+      before { ArticleJSON.configure { |c| c.facebook_token = token } }
+
+      it { should eq token }
+    end
   end
 end
