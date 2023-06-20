9465af2b8a48b496c11d0f46e0394b71b7cddd26
Damien Dillon <damien.dillon@devex.com>
2023-06-20 16:03:50 +0200

    Apply Rubocop fixes

--- lib/article_json/export/apple_news/elements/text.rb
+++ lib/article_json/export/apple_news/elements/text.rb
@@ -9,7 +9,7 @@ module ArticleJSON
           # A Nokogiri object is returned with`super`, which is is then
           # returned as a either a string or as HTML (when not plain text),
           # both of which are compatible with Apple News format. Takes into
-          #  account bold, italic and href.
+          # account bold, italic and href.
           # @return [String]
           def export
             super.to_s
